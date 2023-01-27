f4b0fdfa0319f16aeaee8eefe773bff065213ee4
Dmytro Zhelieznyi <qozzez1337@gmail.com>
2023-01-27 18:30:22 +0100

    init commit

---
+++ src/main/java/com/qozz/leword/LewordApplication.java
@@ -0,0 +1,13 @@
+package com.qozz.leword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LewordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LewordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qozz/leword/LewordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qozz.leword;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LewordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
