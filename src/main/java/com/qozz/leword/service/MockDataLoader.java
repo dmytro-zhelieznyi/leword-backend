d869f4ab11343da5a8ec9f01a8f682a3ca5925b9
Dmytro Zhelieznyi <qozzez1337@gmail.com>
2023-02-13 19:19:40 +0100

    [feat] connected to test db

--- src/main/java/com/qozz/leword/service/MockDataLoader.java
+++ src/main/java/com/qozz/leword/service/MockDataLoader.java
@@ -43,57 +43,57 @@ public class MockDataLoader {
         userRepository.save(user);
         userRepository.save(admin);
 
-        List<Category> categories = LongStream.rangeClosed(1, 6)
-                .boxed()
-                .map(num -> Category.builder().id(num)
-                        .valueNo("cNo-" + num)
-                        .valueEn("cEn-" + num)
-                        .build())
-                .toList();
-
-        categoryRepository.saveAll(categories);
-
-        List<UserCategory> userCategories = categories.stream()
-                .map(category -> UserCategory.builder()
-                        .id(UserCategoryId.builder()
-                                .userId(category.getId() % 2 == 0 ? 1L : 2L)
-                                .categoryId(category.getId())
-                                .build())
-                        .user(category.getId() % 2 == 0 ? user : admin)
-                        .category(category)
-                        .build())
-                .toList();
-
-        userCategoryRepository.saveAll(userCategories);
-
-        List<Word> words = categories.stream()
-                .flatMap(category -> LongStream.rangeClosed(1, 3)
-                        .boxed()
-                        .map(num -> Word.builder()
-                                .id(null)
-                                .valueNo("c" + category.getId() + "-valueNo-" + num)
-                                .valueEn("c" + category.getId() + "-valueEn-" + num)
-                                .category(category)
-                                .build()))
-                .toList();
-
-        wordRepository.saveAll(words);
-
-        List<UserWord> userWords = words.stream()
-                .map(word -> UserWord.builder()
-                        .id(UserWordId.builder()
-                                .userId(word.getId() % 2 == 0 ? 1L : 2L)
-                                .wordId(word.getId())
-                                .build())
-                        .user(word.getId() % 2 == 0 ? user : admin)
-                        .word(word)
-                        .repeat((int) (word.getId() % 2))
-                        .lastRepeatTime(LocalDateTime.now())
-                        .nextRepeatTime(word.getId() % 3 == 0 ? LocalDateTime.now() : LocalDateTime.now().plusHours(1L))
-                        .build())
-                .toList();
-
-        userWordRepository.saveAll(userWords);
+//        List<Category> categories = LongStream.rangeClosed(1, 6)
+//                .boxed()
+//                .map(num -> Category.builder().id(num)
+//                        .valueNo("cNo-" + num)
+//                        .valueEn("cEn-" + num)
+//                        .build())
+//                .toList();
+//
+//        categoryRepository.saveAll(categories);
+//
+//        List<UserCategory> userCategories = categories.stream()
+//                .map(category -> UserCategory.builder()
+//                        .id(UserCategoryId.builder()
+//                                .userId(category.getId() % 2 == 0 ? 1L : 2L)
+//                                .categoryId(category.getId())
+//                                .build())
+//                        .user(category.getId() % 2 == 0 ? user : admin)
+//                        .category(category)
+//                        .build())
+//                .toList();
+//
+//        userCategoryRepository.saveAll(userCategories);
+//
+//        List<Word> words = categories.stream()
+//                .flatMap(category -> LongStream.rangeClosed(1, 3)
+//                        .boxed()
+//                        .map(num -> Word.builder()
+//                                .id(null)
+//                                .valueNo("c" + category.getId() + "-valueNo-" + num)
+//                                .valueEn("c" + category.getId() + "-valueEn-" + num)
+//                                .category(category)
+//                                .build()))
+//                .toList();
+//
+//        wordRepository.saveAll(words);
+//
+//        List<UserWord> userWords = words.stream()
+//                .map(word -> UserWord.builder()
+//                        .id(UserWordId.builder()
+//                                .userId(word.getId() % 2 == 0 ? 1L : 2L)
+//                                .wordId(word.getId())
+//                                .build())
+//                        .user(word.getId() % 2 == 0 ? user : admin)
+//                        .word(word)
+//                        .repeat((int) (word.getId() % 2))
+//                        .lastRepeatTime(LocalDateTime.now())
+//                        .nextRepeatTime(word.getId() % 3 == 0 ? LocalDateTime.now() : LocalDateTime.now().plusHours(1L))
+//                        .build())
+//                .toList();
+//
+//        userWordRepository.saveAll(userWords);
 
 
 //        List<UserWord> userWords = words.stream()
